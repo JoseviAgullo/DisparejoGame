9a2b0fdb380e3da2dee9a9148d7258a0b205a57f
Josevi <josevi.agullo@gmail.com>
2014-08-21 12:22:45 +0200

    First test created and several files renamed

--- DisparejoGame/Game.cs
+++ DisparejoGame/Game.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace DisparejoGame
 {
-    class Program
+    public class Game
     {
         static void Main(string[] args)
         {
---
+++ DisparejoGameTest/TestUnits.cs
@@ -0,0 +1,16 @@
+﻿using DisparejoGame;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DisparejoGameTest
+{
+    [TestClass]
+    public class TestUnits
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Game game = new Game();
+        }
+    }
+}
